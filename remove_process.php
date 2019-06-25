21a7fda9a51af3aa7d9769850fb8961c8ce85540
Tanawit Prasertsak <aommerize@gmail.com>
2019-06-25 14:59:49 +0700

    Add remove process

---
+++ remove_process.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="theme.css">
+
+        <title>Import Radius Users : Remove Process</title>
+    </head>
+    <body>
+        <?php
+
+            $conn = mysqli_connect("localhost","root","","testbeenet"); // Conect to MySQL
+            mysqli_set_charset($conn,"utf8"); // MySQL utf-8
+
+                $sql = "SELECT * FROM group"; 
+                $query = mysqli_query($conn,$sql);
+                
+                    // $sql1 = "DELETE FROM testbeenet.group WHERE groupid = '".$objResult[groupid]."'";
+                    $sql1 = "DELETE FROM testbeenet.group WHERE groupid = 1";
+
+                    $query = mysqli_query($conn,$sql1);
+                
+               
+        ?>
+           
+        </div>
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    </body>
+</html>
--- update.php
+++ update.php
@@ -8,7 +8,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Resume - Start Bootstrap Theme</title>
+  <title>Update Radius User</title>
 
   <!-- Bootstrap core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -61,12 +61,13 @@
           <a class="text-success" href="https://www.beenet-engineering.com">Beenet Engineering</a>
         </div>
        
+          
             <h1></h1><h1></h1><h1></h1>
                 <div class="col">
                         <select class="custom-select" name="service_id" id="service_id" required>
-                            <option selected>Update from group name</option>
+                            <option selected>Update from service type</option>
                             <?php
-                                $conn = mysqli_connect("localhost","root","","testbeenet"); // Conect to MySQL
+                                $conn = mysqli_connect("localhost","root","","testbeenet"); // Connect to MySQL
                                 mysqli_set_charset($conn, "utf8");
                                 $sql = "SELECT `groupid`, `name` FROM `group`";
                                 $query = mysqli_query($conn,$sql);
@@ -82,7 +83,7 @@
                         <select class="custom-select" name="service_id" id="service_id" required>
                             <option selected>Update to group name</option>
                             <?php
-                               $conn = mysqli_connect("localhost","root","","testbeenet"); // Conect to MySQL
+                               $conn = mysqli_connect("localhost","root","","testbeenet"); // Connect to MySQL
                                 mysqli_set_charset($conn, "utf8");
                                 $sql = "SELECT `groupid`, `name` FROM `group`";
                                 $query = mysqli_query($conn,$sql);
@@ -100,7 +101,7 @@
                         <button type="submit" class="btn btn-primary">Update</button>
                     </div>
                 </div>
-                
+                              </form>
 
          <!--        <h1></h1><h1></h1><h1></h1><h1></h1><h1></h1><h1></h1><h1></h1>      
                 <button type="button" class="btn btn-primary" onclick="window.location.href='upload_form.php'">Upload</button>  -->
