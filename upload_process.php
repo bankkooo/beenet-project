d4e1262c5a9d6692d7d1d51d207d11bdd99a10b5
chawallid <22177340+chawallid@users.noreply.github.com>
2019-07-03 14:13:09 +0700

    ui

--- upload_process.php
+++ upload_process.php
@@ -1,17 +1,28 @@
 <!doctype html>
 <html lang="en">
     <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons">
+        <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons">
+        <link rel="stylesgeet" href="https://rawgit.com/creativetimofficial/material-kit/master/assets/css/material-kit.css">
+            <!--Bootsrap 4 CDN-->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="theme.css">
+        
+        <!--Fontawesome CDN-->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
-        <title>Import Radius Users : Upload Process</title>
+        <!--Custom styles-->
+        <link rel="stylesheet" type="text/css" href="styles.css">
+            
+        
+
+        <title>Upload Process</title>
     </head>
-    <body>
+    <body class="profile-page" >
+        <br>
+        <div class="container">
+            <div class="d-flex justify-content-center h-100">
         <?php
             setlocale ( LC_ALL, 'en_US.UTF-8' ); // fgetcsv utf-8
             
@@ -51,59 +62,73 @@
                     $count++;
                 }
             }
-            //fclose($obj_csv);
-            
+        
             $row = $count + $dup_count;
             $perc_succ = ($count/$row)*100;
             $perc_dup = ($dup_count/$row)*100;
         ?>
-
-        <div class="container border border-light my-md-5 p-4">
-            <div class="row">
-                <div class="col text-center">
-                    <span class="text-success"><?php echo "Success import: ".$count." records";?></span>
-                    <span> | </span>
-                    <span class="text-danger"><?php echo "Duplicate import: ".$dup_count." records";?></span>
+         <div class="card">
+                    <div class="card-header">
+                        <div class="center">
+                        <h3>UPDATE PROCESS</h3>
+                        </div>
+                        
+                    </div>
+                    <div class="card-body">
+                            <div class="row">
+                                <div class="col text-center">
+                                    <span class="text-success"><?php echo "Success import: ".$count." records";?></span>
+                                    <span> | </span>
+                                    <span class="text-danger"><?php echo "Duplicate import: ".$dup_count." records";?></span>
+                                </div>
+                            </div>
+                            <div class="progress mb-4" style="height: 20px;">
+                                <div class="progress-bar bg-success" role="progressbar" style="width: <?php echo number_format((float)$perc_succ, 2, '.', '') . "%"; ?>" 
+                                    aria-valuenow="<?php echo number_format((float)$perc_succ, 2, '.', ''); ?>" aria-valuemin="0" aria-valuemax="100">
+                                    <?php echo number_format((float)$perc_succ, 2, '.', '') . " %"; ?>
+                                </div>
+                                <div class="progress-bar bg-danger" role="progressbar" style="width: <?php echo number_format((float)$perc_dup, 2, '.', '') . "%"; ?>" 
+                                    aria-valuenow="<?php echo number_format((float)$perc_dup, 2, '.', ''); ?>" aria-valuemin="0" aria-valuemax="100">
+                                    <?php echo number_format((float)$perc_dup, 2, '.', '') . " %"; ?>
+                                </div>
+                            </div>
+                            <?php if($dup_count > 0) {?>
+                            <p>
+                                <div class="col text-center">
+                                   <button class="btn btn-danger" type="button" data-toggle="collapse" data-target="#collapse_dup_users" aria-expanded="false" aria-controls="collapse_dup_users">
+                                    Show Duplicate Users
+                                </button>
+                                </div>
+                                
+                            </p>
+                            <div class="collapse mb-4" id="collapse_dup_users">
+                                <div class="card card-body">
+                                    <!-- Duplicate users -->
+                                    <?php
+                                        $chk = true;
+                                        foreach ($dup_arr as $dup_uname) {
+                                            if($chk){
+                                                echo "Already exist user(s): ".$dup_uname;
+                                                $chk = false;
+                                            }else{
+                                                echo ", ".$dup_uname;
+                                            }
+                                        }
+                                    ?>
+                                </div>
+                            </div>
+                            <?php } ?>
+                            <div class="row">
+                            <div class="col-sm-3"></div>
+                    <div class="col-sm-3"></div>
+                    <div class="col-sm-3"></div>
+                    <div class="col-sm-3"><a class="btn btn-primary" href="upload.php" role="button">Back</a></div>
+                            </div>
+                        
+                    
+                    </div>
                 </div>
-            </div>
-            <div class="progress mb-4" style="height: 20px;">
-                <div class="progress-bar bg-success" role="progressbar" style="width: <?php echo number_format((float)$perc_succ, 2, '.', '') . "%"; ?>" 
-                    aria-valuenow="<?php echo number_format((float)$perc_succ, 2, '.', ''); ?>" aria-valuemin="0" aria-valuemax="100">
-                    <?php echo number_format((float)$perc_succ, 2, '.', '') . " %"; ?>
-                </div>
-                <div class="progress-bar bg-danger" role="progressbar" style="width: <?php echo number_format((float)$perc_dup, 2, '.', '') . "%"; ?>" 
-                    aria-valuenow="<?php echo number_format((float)$perc_dup, 2, '.', ''); ?>" aria-valuemin="0" aria-valuemax="100">
-                    <?php echo number_format((float)$perc_dup, 2, '.', '') . " %"; ?>
-                </div>
-            </div>
-            <?php if($dup_count > 0) {?>
-            <p>
-                <button class="btn btn-danger" type="button" data-toggle="collapse" data-target="#collapse_dup_users" aria-expanded="false" aria-controls="collapse_dup_users">
-                    Show Duplicate Users
-                </button>
-            </p>
-            <div class="collapse mb-4" id="collapse_dup_users">
-                <div class="card card-body">
-                    <!-- Duplicate users -->
-                    <?php
-                        $chk = true;
-                        foreach ($dup_arr as $dup_uname) {
-                            if($chk){
-                                echo "Already exist user(s): ".$dup_uname;
-                                $chk = false;
-                            }else{
-                                echo ", ".$dup_uname;
-                            }
-                        }
-                    ?>
-                </div>
-            </div>
-            <?php } ?>
-            <div class="row">
-                <div class="col text-center">
-                    <a class="btn btn-primary" href="upload.php" role="button">Back</a>
-                </div>
-            </div>
+        </div>
         </div>
 
         <!-- Optional JavaScript -->
